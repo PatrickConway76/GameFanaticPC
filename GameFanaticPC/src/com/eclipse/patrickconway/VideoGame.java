9b8c4a7dd184834569291f034d7fc968dca0b825
Patrick Conway <patriconway@my.lonestar.edu>
2017-05-09 23:34:31 -0500

    Final project complete

--- GameFanaticPC/src/com/eclipse/patrickconway/VideoGame.java
+++ GameFanaticPC/src/com/eclipse/patrickconway/VideoGame.java
@@ -3,6 +3,11 @@ package com.eclipse.patrickconway;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * Stores a video game and related information
+ * @author Patrick
+ *
+ */
 public class VideoGame
 {
 	private String title;
@@ -10,96 +15,161 @@ public class VideoGame
 	private LocalDate releaseDate;
 	private float rating;
 	
+	/**
+	 * Gets the title of the game
+	 * @return The title of the game
+	 */
 	public String getTitle()
 	{
 		return title;
 	}
 	
+	/**
+	 * Sets the title of the game
+	 * @param title The title of the game
+	 */
 	public void setTitle(String title)
 	{
 		this.title = title;
 	}
-
+	
+	/**
+	 * Gets the price of the game
+	 * @return The price of the game
+	 */
 	public float getPrice()
 	{
 		return price;
 	}
-
+	
+	/**
+	 * Sets the price of the game
+	 * @param price The price of the game
+	 */
 	public void setPrice(float price)
 	{
 		this.price = price;
 	}
-
+	
+	/**
+	 * Gets the release date of the game
+	 * @return The release date of the game
+	 */
 	public LocalDate getReleaseDate()
 	{
 		return releaseDate;
 	}
-
+	
+	/**
+	 * Sets the release date of the game
+	 * @param releaseDate The release date of the game
+	 */
 	public void setReleaseDate(LocalDate releaseDate)
 	{
 		this.releaseDate = releaseDate;
 	}
 
+	/**
+	 * Gets the rating of the game
+	 * @return The rating of the game
+	 */
 	public float getRating()
 	{
 		return rating;
 	}
-
-	public void setRating(int rating)
+	
+	/**
+	 * Sets the rating of the game
+	 * @param rating The rating of the game
+	 */
+	public void setRating(float rating)
 	{
 		this.rating = rating;
 	}
-	
+
+	/**
+	 * Constructs a VideoGame object with the title, price, release date, and rating set
+	 * @param title The title of the game
+	 * @param price The price of the game
+	 * @param releaseDate The release date of the game
+	 * @param rating The rating of the game
+	 */
 	public VideoGame(String title, float price, LocalDate releaseDate, float rating)
 	{
-		this.title = title;
-		this.price = price;
-		this.releaseDate = releaseDate;
-		this.rating = rating;
+		setTitle(title);
+		setPrice(price);
+		setReleaseDate(releaseDate);
+		setRating(rating);
 	}
 	
+	/**
+	 * Parameterless constructor. These defaults are used to check if something was entered correctly
+	 */
+	public VideoGame()
+	{
+		setTitle("");
+		setPrice(-1.0f);
+		setReleaseDate(null);
+		setRating(-1.0f);
+	}
+	
+	/**
+	 * Calculates the trade in value for a particular game
+	 * @return The final trade in value of a game
+	 */
 	public float calculateTradeInValue()
 	{
 		long daysSinceRelease;
 		int percentageMultiplier;
 		float tradeInValue;
 		
-		final int DEGRATION_DAYS = 60;
-		final float PERCENTAGE_DISCOUNTED = 0.10f;
-		final float LESS_THAN_60_DAYS_PERCENTAGE = 0.05f;
+		final int DEGRADATION_DAYS = 60; // Number of days before the trade in value drops off
+		final float PERCENTAGE_OFF = 0.10f; // Percentage the trade in value drops off
+		final float LESS_THAN_DEGRADATION_DAYS_PERCENTAGE = 0.05f; // Percentage to take off if before 60 days
 		
+		// Calculates the number of days between the release date of the game, and today's date
 		daysSinceRelease = ChronoUnit.DAYS.between(releaseDate, LocalDate.now());
-		percentageMultiplier = (int) daysSinceRelease / DEGRATION_DAYS;
+		
+		// We have a default percentage, but after a certain amount of days, that percentage increases 
+		// So we have a percentage multiplier which increases the percentage by how many multiples of days that we have
+		percentageMultiplier = (int) (daysSinceRelease / DEGRADATION_DAYS);
 		
 		if (percentageMultiplier == 0)
 		{
-			tradeInValue = price - (LESS_THAN_60_DAYS_PERCENTAGE * price);
+			// We should still take some money off when they trade-in is before DEGRADATION_DAYS
+			tradeInValue = this.getPrice() - (LESS_THAN_DEGRADATION_DAYS_PERCENTAGE * this.getPrice());
 		}
 		else
 		{
-			tradeInValue = price - ((percentageMultiplier * PERCENTAGE_DISCOUNTED) * price);
+			tradeInValue = this.getPrice() - ((percentageMultiplier * PERCENTAGE_OFF) * this.getPrice());
 		}
 		
-		if (tradeInValue < 0 && rating < 8.5)
+		if (tradeInValue < 0 && this.getRating() < 8.5)
 		{
-			return 0.0f; // The customer can't owe us money (unless it's E.T. for the Atari 2600), so just say it's worth nothing.
+			// The customer can't owe us money (unless it's E.T. for the Atari 2600), so just say it's worth nothing.
+			tradeInValue = 0.0f;
 		}
-		else if (rating >= 8.5)
+		else if (this.getRating() >= 8.5)
 		{
 			float tempTradeInValue = tradeInValue + 5; // Don't want to modify trade in value until we know the exact bonus
 			
+			// If trade in value is still less than 0 after $5 increase
 			if (tempTradeInValue < 0)
 			{
-				System.out.println(title + " 0.0");
-				return 0.0f;
+				tradeInValue = 0.0f;
 			}
-			else if (tempTradeInValue > price)
+			else if (tempTradeInValue > this.getPrice())
 			{
-				tradeInValue = price;
+				// The trade in value shouldn't be able to exceed the price. Otherwise people could just buy something and trade it in for more money.
+				tradeInValue = this.getPrice();
+			}
+			else if (tempTradeInValue < this.getPrice())
+			{
+				tradeInValue = tempTradeInValue;
 			}
 		}
 		
-		System.out.println(title + " " + tradeInValue);
+		System.out.println("The trade-in value for " + title + " is: $" + String.format("%.2f", tradeInValue));
 		
 		return tradeInValue;
 	}
--- GameFanaticPC/src/com/eclipse/patrickconway/VideoGameDemo.java
+++ GameFanaticPC/src/com/eclipse/patrickconway/VideoGameDemo.java
@@ -14,9 +14,9 @@ public class VideoGameDemo {
 		
 		// Create the video game list and add in some default data
 		ArrayList<VideoGame> gameList = new ArrayList<>();
-		VideoGame vg1 = new VideoGame("Game 1", 69.96f, LocalDate.now(), 10);
-		VideoGame vg2 = new VideoGame("Game 2", 20.69f, LocalDate.now().minus(5, ChronoUnit.DAYS), 50);
-		VideoGame vg3 = new VideoGame("Game 3", 25.68f, LocalDate.now().minus(9999, ChronoUnit.DAYS), 8.5f);
+		VideoGame vg1 = new VideoGame("Prey", 59.99f, LocalDate.of(2017, 05, 9), 8.5f);
+		VideoGame vg2 = new VideoGame("Horizon Zero Dawn", 59.99f, LocalDate.of(2017, 02, 28), 9.0f);
+		VideoGame vg3 = new VideoGame("Halo 5", 40.00f, LocalDate.of(2015, 10, 27), 7.0f);
 		gameList.add(vg1);
 		gameList.add(vg2);
 		gameList.add(vg3);
@@ -39,7 +39,10 @@ public class VideoGameDemo {
 					calculateTotalTradeInValue(gameList); // Calculates the total trade in value from all games in the list
 					break;
 				case 3:
-					keepGoing = false; // Signals that we're done and need to exit
+					viewGames(gameList); // Signals that we're done and need to exit
+					break;
+				case 4:
+					keepGoing = false;
 					break;
 			}
 		}
@@ -47,32 +50,38 @@ public class VideoGameDemo {
 		System.out.println("Thank you for using the Game Fanatic trade-in system.");
 	}
 	
+	public static void viewGames(ArrayList<VideoGame> gameList)
+	{
+		for (int i = 0; i < gameList.size(); i++)
+		{
+			System.out.println("Title: " + gameList.get(i).getTitle());
+			System.out.println("Price: $" + gameList.get(i).getPrice());
+			System.out.println("Release Date: " + gameList.get(i).getReleaseDate());
+			System.out.println("Rating: " + gameList.get(i).getRating());
+			System.out.println("");
+		}
+	}
 	// Calculates the total trade in value by taking the trade in value for each game and adding it to the list
-	public static float calculateTotalTradeInValue(ArrayList<VideoGame> gameList)
+	public static void calculateTotalTradeInValue(ArrayList<VideoGame> gameList)
 	{
 		float totalValue = 0;
 		
 		for (int i = 0; i < gameList.size(); i++)
 		{
 			totalValue += gameList.get(i).calculateTradeInValue(); // Calls the method the generate the trade in values and adds it to the total
-			System.out.println(totalValue);
 		}
-		return totalValue;
+		System.out.println("The total trade-in value for your " + gameList.size() + " games is $" + String.format("%.2f", totalValue) + ".");
 	}
 	
 	// Menu for entering the game info
 	public static VideoGame enterGame()
 	{
-		// These defaults help identify 1st time and invalid input
-		String title = "";
-		float price = -1.0f;
-		float rating = -1.0f;
+		// Needed to parse date and send invalid date to exception handler
 		String date = "";
-		LocalDate releaseDate = null;
 		
 		boolean isComplete = false;			
 		Scanner sc = new Scanner(System.in);
-		VideoGame newGame = null;
+		VideoGame newGame = new VideoGame();
 		
 		// Loop forever until the user enters valid info for each field
 		while (isComplete == false)
@@ -82,48 +91,51 @@ public class VideoGameDemo {
 				// Each field uses the same pattern, except for string fields since they could be anything
 				// If the field is empty, -1, or null then that signals that the field hasn't been correctly filled out, otherwise it's valid input
 				// This allows us to skip asking for the name again if the user inputs an invalid date for example
-				if (title.isEmpty())
+				if (newGame.getTitle().isEmpty())
 				{
 					System.out.print("Please enter the game title: ");
-					title = sc.nextLine();
+					newGame.setTitle(sc.nextLine());
 				}
 				
-				if (price == -1)
+				if (newGame.getPrice() == -1.0f)
 				{
 					System.out.print("Please enter the game price: ");
-					price = sc.nextFloat();
+					newGame.setPrice(sc.nextFloat());
 					
 					// Since a negative number would be considered 'good' input, we need to make sure the user puts in something greater than 0
 					// If it's less than 0 than throw an exception with a reason and ask them again
-					if (price <= 0)
+					if (newGame.getPrice() <= 0.0f)
 					{
-						price = -1;
-						String message = "The price must be greater than 0";
+						newGame.setPrice(-1.0f);
+						String message = "The price must be greater than 0. Please try again.";
 						throw new InputMismatchException(message);
 					}
 				}
 				
-				if (releaseDate == null)
+				if (newGame.getReleaseDate() == null)
 				{
 					System.out.print("Please enter the release date in YYYY-MM-DD format: ");
 					date = sc.next();
-					releaseDate = LocalDate.parse(date); // This throws a DateTimeParseException if we can't parse the date
+					newGame.setReleaseDate(LocalDate.parse(date)); // This throws a DateTimeParseException if we can't parse the date
 				}
 				
-				if (rating == -1)
+				if (newGame.getRating() == -1.0f)
 				{
 					System.out.print("Please enter the rating for this game between 1-10: ");
-					rating = sc.nextInt();
+					newGame.setRating(sc.nextInt());
 					
 					// Check to see if the rating is between 1 and 10
 					// Throws a new exception with a custom error message if it's not
-					if (rating < 1 || rating > 10)
+					if (newGame.getRating() < 1 || newGame.getRating() > 10)
 					{
-						rating = -1;
+						newGame.setRating(-1.0f);
 						String message = "The rating must be between 1-10. Please try again.";
 						throw new InputMismatchException(message);
 					}
 				}
+				
+				isComplete = true; // If we've gotten this far, it means we're done
+				//newGame = new VideoGame(title, price, releaseDate, rating);
 			}
 			
 			// Comes up when the user enters in a string instead of numbers, or if the input is not in the range that we won't
@@ -148,13 +160,10 @@ public class VideoGameDemo {
 				System.out.println(date + " is invalid input. Please try again.");
 			}
 			
-			isComplete = true; // If we've gotten this far, it means we're done
-			newGame = new VideoGame(title, price, releaseDate, rating);
 		}
-
-		System.out.println(newGame.getTitle() + "\n" + newGame.getPrice() + "\n" + newGame.getReleaseDate().toString() + "\n" + newGame.getRating());
-		
 		return newGame;
+		
+
 	}
 	
 	public static int MainMenu()
@@ -167,11 +176,11 @@ public class VideoGameDemo {
 		{
 			try
 			{
-				System.out.print("Please enter 1 to enter in a game, 2 to display total trade in values, or 3 to exit: ");
+				System.out.print("Please enter 1 to enter in a game, 2 to display total trade in values, 3 to display the list of games in the queue, or 4 to exit: ");
 				input = sc.nextInt();
 				isValidInput = true;
 				
-				if (input < 0 || input > 3)
+				if (input < 0 || input > 4)
 				{
 					System.out.println(input + " is invalid input. Please try again.");
 				}
